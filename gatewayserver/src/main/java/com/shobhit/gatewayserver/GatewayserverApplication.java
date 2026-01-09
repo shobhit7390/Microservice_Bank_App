eb1df34330cc5c219a8099e7a1b84ed4b4381f9a
shobhit6402 <vms6402@gmail.com>
2026-01-09 10:51:38 +0530

    Custom routing for gateway

--- gatewayserver/src/main/java/com/shobhit/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/shobhit/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,33 @@ package com.shobhit.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator fincrestRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+					.path("/fincrest/accounts/**")
+					.filters(f -> f.rewritePath("/fincrest/accounts/(?<segment>.*)", "/${segment}"))
+					.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/fincrest/loans/**")
+						.filters(f -> f.rewritePath("/fincrest/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/fincrest/cards/**")
+						.filters(f -> f.rewritePath("/fincrest/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.build();
 	}
 
 }
