42adbe0a62ec015f5a7e519402354b74e53fd989
shobhit6402 <vms6402@gmail.com>
2026-01-11 00:31:34 +0530

    Circuit breaker in gateway

--- gatewayserver/src/main/java/com/shobhit/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/shobhit/gatewayserver/GatewayserverApplication.java
@@ -21,7 +21,9 @@ public class GatewayserverApplication {
 				.route(p -> p
 					.path("/fincrest/accounts/**")
 					.filters(f -> f.rewritePath("/fincrest/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
 					.uri("lb://ACCOUNTS"))
 				.route(p -> p
 						.path("/fincrest/loans/**")
