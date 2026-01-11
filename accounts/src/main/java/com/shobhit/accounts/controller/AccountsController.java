35a1a6dead1d3e099ee27602120cc368e0d92b15
shobhit6402 <vms6402@gmail.com>
2026-01-11 19:18:58 +0530

    rate limiter in accounts ms

--- accounts/src/main/java/com/shobhit/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/shobhit/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.shobhit.accounts.dto.CustomerDto;
 import com.shobhit.accounts.dto.ErrorResponseDto;
 import com.shobhit.accounts.dto.ResponseDto;
 import com.shobhit.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -217,11 +218,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
     }
 
     @Operation(
